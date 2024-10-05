adc6143b4db088995a624ab19a1b3a20ececdc61
ash4rk <cool.shark.98@mail.ru>
2024-10-06 02:57:53 +0300

    feat(wip): extract Carousel scene, set gen spots and rotation

---
+++ src/environment/carousel.gd
@@ -0,0 +1,24 @@
+extends Node3D
+
+@export var ENEMY_SPOT_HEIGHT: float = -0.658
+@export var RADIUS: float = 1.51
+# TODO: Replace enemy scene by spot scene
+@export var SPOT_SCENE: PackedScene
+@export var ROTATION_SPEED: float = 0.2
+
+func _ready() -> void:
+	gen_enemy_spots(RADIUS, 12, SPOT_SCENE)
+
+func _process(delta: float) -> void:
+	rotate_object_local(Vector3.UP, -ROTATION_SPEED * delta)
+
+func gen_enemy_spots(radius: float, spots_num: int, spot_scene: PackedScene) -> void:
+	var angle_step = 360.0 / spots_num
+	for i in range(spots_num):
+		var angle_radians = deg_to_rad(i * angle_step)
+		var x = radius * cos(angle_radians)
+		var z = radius * -sin(angle_radians)
+		var instance = spot_scene.instantiate()
+		add_child(instance)
+		instance.position = Vector3(x, ENEMY_SPOT_HEIGHT, z)
+		instance.rotation.y = (angle_radians + deg_to_rad(90))
