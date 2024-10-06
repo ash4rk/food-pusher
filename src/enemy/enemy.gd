1ac9ebd84e5717d2f56740db050bb6cf38efc49c
ash4rk <cool.shark.98@mail.ru>
2024-10-06 19:48:42 +0300

    feat: randomize enemy models

--- src/enemy/enemy.gd
+++ src/enemy/enemy.gd
@@ -3,8 +3,10 @@ extends StaticBody3D
 # TODO: Randomize mesh from assets/models/kenney_characters/*.glb
 const MAX_HEALTH: int = 5
 const BOX_POKE_LENGTH: float = 1.4
+const MODELS_PATH: String = "res://assets/models/kenney_characters/"
 
 @onready var DATA_RES: Resource = load("res://data/valuables.tres")
+@onready var startup_mesh: Node3D = $CharacterMesh
 @onready var anim_player: AnimationPlayer = $CharacterMesh/AnimationPlayer
 @onready var food_detector: Area3D = $FoodDetector
 
@@ -18,10 +20,22 @@ var health = MAX_HEALTH :
 		health = max(new_value, 0)
 
 func _ready() -> void:
+	_set_random_mesh()
 	anim_player.animation_set_next("emote-no", "idle")
 	anim_player.play("idle", -1, 0.65)
 	anim_player.animation_finished.connect(_anim_sequences)
 	food_detector.body_entered.connect(_on_body_entered)
+
+func _set_random_mesh():
+	var model_paths = Utilities.get_files_by_filter(MODELS_PATH, ".glb")
+	var rand_model_path = model_paths[randi() % model_paths.size()]
+	var model_instance = load(rand_model_path).instantiate()
+	self.add_child(model_instance)
+	anim_player = model_instance.get_node_or_null("AnimationPlayer")
+	if model_instance == null or anim_player == null:
+		push_error("Invalid model instasnce: %s from path: %s" % [model_instance, rand_model_path])
+		return
+	startup_mesh.queue_free()
 
 func _on_body_entered(body: Node3D):
 	if body.is_in_group("foods"):
