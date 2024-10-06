0fa1c5a94418809fbc904569d0d84d2658e78de9
ash4rk <cool.shark.98@mail.ru>
2024-10-06 08:07:34 +0300

    feat: upgrade debug cam add fov on wheel

--- src/debug/debug.gd
+++ src/debug/debug.gd
@@ -8,3 +8,8 @@ func _input(event: InputEvent) -> void:
 	
 	if Input.is_key_pressed(KEY_C):
 		debug_cam.current = !debug_cam.current
+
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_WHEEL_DOWN):
+		debug_cam.fov = min(debug_cam.fov+5, 150)
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_WHEEL_UP):
+		debug_cam.fov = max(debug_cam.fov-5, 5)
