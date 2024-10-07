c291c33f794de505ea80cd4d7512461e90e8e334
ash4rk <cool.shark.98@mail.ru>
2024-10-07 13:40:44 +0300

    refactor: remove warning ignores in the name of beauty

--- src/static/event_bus.gd
+++ src/static/event_bus.gd
@@ -1,10 +1,6 @@
 extends Node
 
-@warning_ignore("unused_signal")
 signal on_promos_changed(new_value: int)
-@warning_ignore("unused_signal")
 signal on_rewards_changed(new_value: int)
-@warning_ignore("unused_signal")
 signal on_drowned_with_reward(reward_value: int)
-@warning_ignore("unused_signal")
 signal on_remaining_throws_changed(new_value: int)
